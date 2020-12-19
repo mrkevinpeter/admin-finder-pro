93f41eb22435650d3c64f6435ea13a47cd353a71
MrKevinPeter <mrkevinpeter.info@gmail.com>
2020-12-19 16:08:02 +0500

    files

---
+++ README.md
@@ -0,0 +1,31 @@
+# Admin Finder Pro
+Admin Finder Pro Script in python.
+I can surely say that it will be the best admin finder you ever used.Admin finder pro contains both custom and default (pr-installed) wordlists.Admin finder pro is written in python and is much faster.It can many type of wordlists like for .html, .asp ,.php , .cfm and many more.I suggest to use it once...
+Written in {Python3}
+Written by [Kevin Peter](https://github.com/mrkevinpeter)
+
+# Using Script
+Command on Linux Terminal/Termux:
+1)type "cd admin-finder-pro" to into adminfinderpro directory.
+2)type "python  admin-page-finder.py" to run the script.
+3)Give url as (www.example.com or example.com) without ending slash
+4)type the  number according to your choice either if you want to use your own wordlist or default (pre-installed)
+5)Choose the type of login page you want to find.
+6)Now it wiil start its work and there will be a beep and it will done
+# Git Link
+[https://github.com/mrkevinpeter/admin-finder-pro](https://github.com/mrkevinpeter/admin-finder-pro)
+git clone [https://github.com/mrkevinpeter/admin-finder-pro](https://github.com/mrkevinpeter/admin-finder-pro)
+
+# MrKevinPeter
+T
+# Follow me!
+Instagram: [@MrKevinPeter](https://instagram.com/mrkevinpeter)
+Facebook: [@MrKevinPeter](https://facebook.com/mrkevinpeter)
+Twitter: [@MrKevinPeter](https://twitter.com/mrkevinpeter)
+LinkedIn: [@MrKevinPeter](https://linkedin.com/mrkevinpeter)
+
+#YOUTUBE
+[Subscribe Now](https://www.youtube.com/channel/hackerlaboratory) 
+
+# Contact
+Email: [Send Mail](mailto:mrkevinpeter.info@Gmail.Com)
---
+++ adminfinder.py
@@ -0,0 +1,87 @@
+import requests,sys
+
+print("""
+                               (                            
+   (     (                     )\ )           (             
+   )\    )\ )   )   (         (()/( (         )\ )  (  (    
+((((_)( (()/(  (    )\  (      /(_)))\  (    (()/( ))\ )(   
+ )\ _ )\ ((_)) )\  ((_) )\ )  (_))_((_) )\ )  ((_)/((_(()\  
+ (_)_\(_)_| |_((_)) (_)_(_/(  | |_  (_)_(_/(  _| (_))  ((_) 
+  / _ \/ _` | '  \()| | ' \)) | __| | | ' \)/ _` / -_)| '_| 
+ /_/ \_\__,_|_|_|_| |_|_||_|  |_|   |_|_||_|\__,_\___||_|   
+                                                            
+	""")
+print("TY:Hackerslaboratory  SocialMedia:@MrKevinPeter\n\n")
+
+
+
+url=input("Enter the Website URL: ")
+
+def kevin():
+	lives=[]
+	lines=arq.readlines()
+	arq.close
+	fd="Not Found"
+	fdb = str.encode(fd)
+
+	for line in lines:
+		line =line.replace("\n", "")
+		request = url+'/'+line
+		http = requests.get(request)
+		code = http.status_code
+		if code != 301 and code!= 404:
+			if not fdb in http.content :
+				print("[+] Page Found: "+request)
+				lives.append(request)
+			else:
+				print("[-] Page Not Found: "+request)
+		else:
+			print("[-] Page Not Found: "+ request)
+	print("\aFinish!")
+	for live in lives:
+		print(live)
+
+
+print("Choose the wordlist Source:\n")
+print("1)Custom Wordlist.")
+print("2)Default Wordlist.")
+scr=int(input(":>"))
+if scr==1:
+	wl=input("Enter a path for wordlist:")
+	arq = open(wl,"r")
+	kevin()
+elif scr==2:
+	print("Select the wordlist type you want:")
+	print("1) HTML")
+	print("2) PHP")
+	print("3) ASP")
+	print("4) CFM")
+	print("5) JS")
+	print("6) CGI")
+	print("7) BRF")
+	scrt=int(input("::>"))
+	if scrt==1:
+		arq = open("sourcefiles\hwtlml.txt","r")
+		kevin()
+	elif scrt==2:
+		arq = open("sourcefiles\pwhlp.txt","r")
+		kevin()
+	elif scrt==3:
+		arq = open("sourcefiles\\awslp.txt","r")
+		kevin()
+	elif scrt==4:
+		arq = open("sourcefiles\cwflm.txt","r")
+		kevin()
+	elif scrt==5:
+		arq = open("sourcefiles\jwsl.txt","r")
+		kevin()
+	elif scrt==6:
+		arq = open("soucrefiles\cwgli.txt","r")
+		kevin()
+	elif scrt==7:
+		arq = open("sourcefiles\\bwrlf.txt","r")
+		kevin()
+	else:
+		print("Invaild Input!")
+else:
+	print("Invaild Input!")
